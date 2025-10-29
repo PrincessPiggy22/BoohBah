17849aae7818e6be456b69d65376d3e5e7ed4bb8
PrincessPiggy22 <mlpuryear@my.waketech.edu>
2025-10-29 08:55:44 -0400

    Create BoohbahDanceTest.java

---
+++ BoohbahDanceLab/BoohbahDanceTest.java
@@ -0,0 +1,12 @@
+public class BoohbahDanceTest {
+    public static void main(String[] args) {
+        Boohbah b1 = new Boohbah("Zing Zing Zingbah","the Worm");
+        Boohbah b2 = new Boohbah("Jingbah","the Chicken Dance");
+        Boohbah b3 = new Boohbah("Humbah","Poke dance");
+
+
+        Boohbah[] theBoohbahs = {b1,b2,b3};
+        new DanceRoutine(theBoohbahs).buildRoutine();
+        new DanceRoutine(theBoohbahs).modifyRoutine();
+    }
+}

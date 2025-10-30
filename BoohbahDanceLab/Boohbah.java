3a403fb6ed24f6bfdc86bbabbfb5e1b812b4444e
PrincessPiggy22 <mlpuryear@my.waketech.edu>
2025-10-30 09:24:49 -0400

    Update Boohbah.java

--- BoohbahDanceLab/Boohbah.java
+++ BoohbahDanceLab/Boohbah.java
@@ -4,6 +4,11 @@ public class Boohbah {
     private String name;
     private String danceMove;
 
+    Boohbah(){ // made this because DanceRoutine wouldn't extend w/o it
+        this.name = "Zing Zing Zingba";
+        this.danceMove = "The Worm";
+    }
+
     Boohbah(String name, String danceMove){
         this.name = name;
         this.danceMove = danceMove;
